f6525bd0deb2bd9625a8c0067fd8e658147dafe9
Jorge Eduardo <jorgeeduardovrb@gmail.com>
2020-12-07 19:56:19 -0300

    Add projeto.

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <h1>Sign up to my newsletter</h1>
+        <p></p>
+        <input type="text"></input>
+        <button>Sign up</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
